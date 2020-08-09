77561eb530507a396d3fc540bf30a89a886a3678
Barbara Kullmann <b.kullmanns@gmail.com>
2020-08-09 01:55:59 -0300

    churras front

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import { Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+import GlobalStyles from "./shared-styles/global.styles";
+import theme from "./shared-styles/theme";
+import Routes from "./routes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <AlertProvider
+        template={AlertTemplate}
+        position="bottom center"
+        timeout={5000}
+      >
+        <Routes />
+      </AlertProvider>
+    </ThemeProvider>
   );
 }
 
